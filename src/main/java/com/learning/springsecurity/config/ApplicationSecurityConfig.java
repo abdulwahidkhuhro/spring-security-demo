8f46a60a1f8edffb6e96f3c2e97a667fd3cafeaa
Abdul Wahid <abdulwahidkhuhro1@gmail.com>
2024-06-28 06:03:00 +0500

    added basic authentication and users in memory for learning purpose

--- src/main/java/com/learning/springsecurity/config/ApplicationSecurityConfig.java
+++ src/main/java/com/learning/springsecurity/config/ApplicationSecurityConfig.java
@@ -1,15 +1,21 @@
 package com.learning.springsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig {
 	
+	@Bean
 	SecurityFilterChain defaulSecurityFilterChain(HttpSecurity http) throws Exception{
 		
 		http
@@ -19,5 +25,20 @@ public class ApplicationSecurityConfig {
 			.httpBasic(Customizer.withDefaults());
 		
 		return http.build();
+	}
+	
+	
+	
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		
+		UserDetails annaSmithUser = User.builder()
+											 .username("annasmith")
+											 .password("password")
+											 .roles("STUDENT")
+											 .build();
+		return new InMemoryUserDetailsManager(
+				annaSmithUser
+				);
 	}
 }
