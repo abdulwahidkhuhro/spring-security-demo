8053b4ec98078fa8cec941d2b62c02a0d4fdf3c7
Abdul Wahid <abdulwahidkhuhro1@gmail.com>
2024-06-30 22:17:20 +0500

    added roles

--- src/main/java/com/learning/springsecurity/config/ApplicationSecurityConfig.java
+++ src/main/java/com/learning/springsecurity/config/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.learning.springsecurity.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,25 +9,38 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.learning.springsecurity.security.ApplicationUserRole;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig {
 	
+	
+	private final PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+	
 	@Bean
 	SecurityFilterChain defaulSecurityFilterChain(HttpSecurity http) throws Exception{
 		
-		http
+		http.csrf().disable()
 			.authorizeHttpRequests(
-					(authorize) -> authorize.anyRequest()
-											.authenticated())
+					(authorize) -> authorize.requestMatchers("/api/**")
+											.hasRole(ApplicationUserRole.STUDENT.name())
+											.anyRequest()
+											.authenticated()
+											)
 			.httpBasic(Customizer.withDefaults());
 		
 		return http.build();
 	}
-	
 	
 	
 	@Bean
@@ -34,11 +48,26 @@ public class ApplicationSecurityConfig {
 		
 		UserDetails annaSmithUser = User.builder()
 											 .username("annasmith")
-											 .password("password")
-											 .roles("STUDENT")
+											 .password(passwordEncoder.encode("password"))
+											 .roles(ApplicationUserRole.STUDENT.name())
 											 .build();
+		
+		UserDetails wahidUser = User.builder()
+											.username("wahid")
+											.password(passwordEncoder.encode("4244"))
+											.roles(ApplicationUserRole.ADMIN.name())
+											.build();
+		
+		UserDetails jameelUser = User.builder()
+				.username("jameel")
+				.password(passwordEncoder.encode("4244"))
+				.roles(ApplicationUserRole.ADMINTRAINEE.name())
+				.build();
+		
 		return new InMemoryUserDetailsManager(
-				annaSmithUser
+				annaSmithUser,
+				wahidUser,
+				jameelUser
 				);
 	}
 }
